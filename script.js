d3f30c40c25a420ded0535a186cff69637615966
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-07-21 09:57:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const slider = document.querySelector('.items');
 let isDown = false;
-let startX;
-let scrollLeft;
+let startX = 0;
+let scrollLeft = 0;
 
 slider.addEventListener('mousedown', (e) => {
   isDown = true;
   slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
+  startX = e.pageX;
   scrollLeft = slider.scrollLeft;
 });
 
@@ -23,7 +23,7 @@ slider.addEventListener('mouseup', () => {
 slider.addEventListener('mousemove', (e) => {
   if (!isDown) return;
   e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const walk = (x - startX) * 2; // scroll speed multiplier
+  const x = e.pageX;
+  const walk = (x - startX) * 1; // Adjust scroll speed if needed
   slider.scrollLeft = scrollLeft - walk;
 });
